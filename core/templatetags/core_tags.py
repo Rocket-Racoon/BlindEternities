5340887bad61217da06e1bb03d14c9be21854733
Héctor Torres <hector.torres@simpat.tech>
2026-03-19 11:49:03 -0600

    Core Templatetags (filters)

---
+++ core/templatetags/core_tags.py
@@ -0,0 +1,26 @@
+from django import template
+from django.utils.safestring import mark_safe
+from core.utils import format_mana_cost
+
+register = template.Library()
+
+
+@register.filter
+def mana_cost(value):
+    return mark_safe(format_mana_cost(value))
+
+
+@register.simple_tag(takes_context=True)
+def is_owner(context, obj):
+    request = context.get('request')
+    if not request or not request.user.is_authenticated:
+        return False
+    return getattr(obj, 'user', None) == request.user
+
+
+@register.simple_tag(takes_context=True)
+def active_url(context, url_name):
+    request = context.get('request')
+    if request and request.resolver_match.url_name == url_name:
+        return 'active'
+    return ''
